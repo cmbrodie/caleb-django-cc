2a12a8a84b4fdc54e285c672ce1883c599042549
cmbrodie <cmbrodiegt@gmail.com>
2022-05-01 23:56:41 -0400

    complete movies application

--- movies/views.py
+++ movies/views.py
@@ -1,27 +1,31 @@
-from django.http import HttpResponse
+from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-
-data = {
-    'movies': [
-        {
-            'id': 5,
-            'title': 'Jaws',
-            'year': 1557
-        },
-        {
-            'id': 7,
-            'title': 'Alien',
-            'year': 1979
-        },
-        {
-            'id': 17,
-            'title': 'The Dark Knight',
-            'year': 2008
-        }
-        ]
-}
+from .models import Movie
 def movies(request):
-    return render(request, 'movies/movies.html', data)
+    data = Movie.objects.all()
+    return render(request, 'movies/movies.html', {'movies':data})
 
 def home(request):
     return HttpResponse("Home Page")
+
+def detail(request, id):
+    data = Movie.objects.get(pk=id)
+    return render(request, 'movies/detail.html', {'movie': data})
+
+def add(request):
+    title = request.POST.get('title')
+    year = request.POST.get('year')
+    if title and year:
+        movie = Movie(title=title, year= year)
+        movie.save()
+        return HttpResponseRedirect('/movies')
+
+    return render(request, 'movies/add.html')
+
+def delete(request, id):
+    try:
+        movie = Movie.objects.get(pk=id)
+    except:
+        raise Http404(f'Movie with id {id} does not exist')
+    movie.delete()
+    return HttpResponseRedirect('/movies')
